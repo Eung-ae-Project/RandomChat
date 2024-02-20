b3922a545f9039156fa34353e7172eed3ebd627e
smg0725 <smg0725@naver.com>
2024-02-20 18:11:21 +0900

    completeNavBar

--- front/src/layouts/Header/index.tsx
+++ front/src/layouts/Header/index.tsx
@@ -1,20 +1,62 @@
 
-import {Head,
-  StyledLink
+import {
+  StyledLink,Head, NavBar,
+
 } from "@layouts/Header/styles"
+import {Link, NavLink, useLocation, useParams} from "react-router-dom";
+import React from "react";
+import {SerializedStyles} from "@emotion/react";
+
+
+interface css{
+  key: string;
+  to: string;
+  css: SerializedStyles | undefined;
+}
 
 const Header =(location:string)=>{
   console.log("헤더!!")
+
+
+  const Bar2 =({location}:any)=>{
+    const selectedPath = location.pathname;
+
+    const links = [
+      { path: '/private', text: '1:1 채팅' },
+      { path: '/group', text: '단체 채팅' },
+      { path: '/myChat', text: '내 채팅' },
+      { path: '/myProfile', text: '내 프로필' }
+    ];
+
+    const activeStyle = {
+      color: 'white',
+      background: '#A4D3FF',
+    }
+    const deActiveStyle = {
+      color: 'rgba(135, 135, 135, 100)',
+    }
+
+    const renderLinks = (selectedPath: string) =>
+        links.map(({ path, text }) => (
+            <NavBar key={path} to={path} style={({isActive}) => {
+              return isActive ? activeStyle : deActiveStyle;
+            }}>
+              {text}
+            </NavBar>
+
+        ));
+
+
+    return <>{renderLinks(selectedPath)}</>;
+  }
+
     return(
-      <>
+      <div>
         <Head>
           <StyledLink to="/group">이런저런 랜덤채팅</StyledLink>
-          <StyledLink to="/private">1:1 채팅</StyledLink>
-          <StyledLink to="/group">단체 채팅</StyledLink>
-          <StyledLink to="/MyChat">내 채팅</StyledLink>
-          <StyledLink to="/MyProfile">내 프로필</StyledLink>
+          <Bar2 location={useLocation()}></Bar2>
         </Head>
-      </>
+      </div>
     )
 };
 
--- front/src/layouts/Header/styles.tsx
+++ front/src/layouts/Header/styles.tsx
@@ -1,36 +1,61 @@
 import styled from '@emotion/styled';
-import { Link } from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
+import {css} from "@emotion/react";
 
 
-export const Head = styled.header`
+export const Head = styled.ul`
   height: 120px;
     font-weight: 1000;
-    font-size: 29px;
-    padding: 0 50px 0;
+    font-size: 39px;
+    margin:0;
+    padding: 0;
+    
     font-style: normal;
     & > :first-of-type {
         /* 첫 번째 자식 요소에 대한 스타일 */
-        margin-right: 75px;
+        flex-basis: 170%;
+        justify-content: start;
+        padding-left: 50px;
     }
 		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		text-decoration:none;
+    justify-content: space-between; /* 가로 방향으로 가운데 정렬 */
+    align-items: center;
+    text-decoration:none;
     border-bottom: 2px solid #A4D3FF; // 테두리 스타일 설정
     
-  text-align: center;
 `;
-export const StyledLink = styled(Link)`
-	color: rgba(135, 135, 135, 100);;
+export const StyledLink = styled(NavLink)`
+	color: rgba(135, 135, 135, 100);
+    width: 100%;
+    height: 100%;
 	text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`;
+export const NavBar = styled(NavLink)`
+	
+    width: 100%;
+    height: 100%;
+	text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+`;
 
-
-    @font-face {
-        font-family: 'BMHANNAPro';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_seven@1.0/BMHANNAPro.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
+export const FocusedLink = styled(Link)`
+	color:white;
+    width: 100%;
+    height: 100%;
+    
+	text-decoration: none;
+    background: #A4D3FF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
 `;
 
 

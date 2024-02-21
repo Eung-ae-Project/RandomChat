18f847a1db606437479f8cf6ce424b488a3134ef
sem201 <sem201@naver.com>
2024-02-21 11:11:22 +0900

    login, sighup 수정

--- front/src/pages/Login/index.tsx
+++ front/src/pages/Login/index.tsx
@@ -1,13 +1,52 @@
-
-
+import React,{useCallback,useState} from 'react';
+import useInput from '../../hooks/useInput';
+import axios from 'axios';
+import {Button, Img, Form, Input, Label} from './styles';
 const Login =()=>{
+	const [logInError, setLogInError] = useState(false);
+	const[id,onchangeId] = useInput('');
+	const[password,onchangePassword] = useInput('');
+	const onSubmit = useCallback(
+		(e:any) =>{
+			e.preventDefault();
+			setLogInError(false);
+			axios
+			.post(
+				'',
+				{id,password},
+				{withCredentials:true,}
+			)
+			.then(()=>{
+
+			})
+			.catch((error)=>{
+				setLogInError(error.response?.status === 401);
+			})
+		},[id,password],
+	);
+	
 
 	return(
-		<>
-			<h1>
-				로그인 페이지
-			</h1>
-		</>
+		<div id ='container'>
+			<Img src="src/assets/logo.png" alt="logo" />
+			<Form onSubmit={onSubmit}>
+				<label id = 'id-label'>
+				<Input type ='id' id='id' name = 'id' value={id} onChange={onchangeId}/>
+				{/* {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>} */}
+				</label>
+				<label id = 'password-label'>
+				<Input type ='password' id='password' name = 'password' value={password} onChange={onchangePassword}/>
+				</label>
+				<Label id='islogin'>
+				<Input type='checkbox' id='islogin'/>로그인 유지하기
+				</Label>
+				<Button id='login' type='submit'>로그인</Button>
+				<Button id ='speclogin'><Img id='kn' src ="src/assets/kakao.png"></Img></Button>
+				<Button id ='speclogin'><Img id='kn' src ="src/assets/naver.png"></Img></Button>
+			</Form>
+			<a href='./signup' id = 'tologin'>회원가입</a>
+			<a href=''id = 'tolookfor'>아이디 /비밀번호 찾기</a>
+		</div>
 	)
 }
 
